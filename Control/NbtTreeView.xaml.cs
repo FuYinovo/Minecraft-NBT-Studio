aa4d929a785f1e8cf7cf5627995acea95f43c5ed
FuYin <2706117703@qq.com>
2025-08-18 18:05:36 +0800

    [重构]「插入节点」右键菜单由NodeMenuFlyoutItem结构体动态生成

--- Control/NbtTreeView.xaml.cs
+++ Control/NbtTreeView.xaml.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Windows.System;
 using CommunityToolkit.Mvvm.Messaging;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
+using NBT_Studio.Library.NBT_Parser.Enum;
 using NBT_Studio.Message;
 using NBT_Studio.Model;
 
@@ -10,27 +13,73 @@ namespace NBT_Studio.Control;
 
 public sealed partial class NbtTreeView
 {
+    // 「插入节点」右键菜单
+    private readonly List<NodeMenuFlyoutItem> _menuFlyoutItems =
+    [
+        new(NbtTagEnum.Byte, VirtualKey.B),
+        new(NbtTagEnum.Short, VirtualKey.S),
+        new(NbtTagEnum.Int, VirtualKey.I),
+        new(NbtTagEnum.Long, VirtualKey.L),
+        new(NbtTagEnum.Float, VirtualKey.F),
+        new(NbtTagEnum.Double, VirtualKey.D),
+        new(NbtTagEnum.String, VirtualKey.S),
+        NodeMenuFlyoutItem.Separator,
+        new(NbtTagEnum.ByteArray, VirtualKey.B, VirtualKeyModifiers.Control),
+        new(NbtTagEnum.IntArray, VirtualKey.I, VirtualKeyModifiers.Control),
+        new(NbtTagEnum.LongArray, VirtualKey.L, VirtualKeyModifiers.Control),
+        NodeMenuFlyoutItem.Separator,
+        new(NbtTagEnum.Dictionary, VirtualKey.D, VirtualKeyModifiers.Control),
+        new(NbtTagEnum.List, VirtualKey.L, VirtualKeyModifiers.Control)
+    ];
+
     public NbtTreeView()
     {
         InitializeComponent();
     }
 
-    /// <summary>
-    ///     展开或折叠子项
-    /// </summary>
+    ///<summary> 展开或折叠子项 </summary>
     private void TreeViewItem_DoubleClicked(object sender, DoubleTappedRoutedEventArgs e)
     {
         if (sender is not TreeViewItem treeViewItem) return;
         treeViewItem.IsExpanded = !treeViewItem.IsExpanded;
     }
 
-    /// <summary>
-    ///     通知「选择节点」修改的消息
-    /// </summary>
+    ///<summary> 通知「选择节点」修改的消息 </summary>
     private void SendSelectedNodeChangedMessage(TreeView sender, TreeViewSelectionChangedEventArgs args)
     {
         WeakReferenceMessenger.Default.Send(
             new SelectedNodeChangedMessage(sender.SelectedNode));
+    }
+
+    ///<summary> 动态生成「插入节点」右键菜单</summary>
+    private void MenuFlyoutSubItem_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (sender is not MenuFlyoutSubItem { DataContext: NbtNode node } menu) return;
+        menu.Items.Clear();
+        var command = node.AppendChildCommand; // Command
+        foreach (var item in _menuFlyoutItems)
+        {
+            if (item.IsSeparator)
+            {
+                menu.Items.Add(new MenuFlyoutSeparator());
+                continue;
+            }
+
+            menu.Items.Add(new MenuFlyoutItem
+            {
+                Text = item.Title,
+                Command = command,
+                CommandParameter = item.CommandParameter,
+                KeyboardAccelerators =
+                {
+                    new KeyboardAccelerator
+                    {
+                        Key = item.Key,
+                        Modifiers = item.ModifierKey
+                    }
+                }
+            });
+        }
     }
 
     #region Properties
---
+++ Model/NodeMenuFlyoutItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+using Windows.System;
+using NBT_Studio.Library.NBT_Parser.Enum;
+
+namespace NBT_Studio.Model;
+
+public struct NodeMenuFlyoutItem
+{
+    public NodeMenuFlyoutItem(NbtTagEnum commandParameter,
+        VirtualKey key = VirtualKey.None,
+        VirtualKeyModifiers modifier = VirtualKeyModifiers.None, ICommand? command = null)
+    {
+        Title = Utils.Tools.GetEnumDescription(commandParameter) ?? commandParameter.ToString();
+        Command = command;
+        CommandParameter = commandParameter;
+        IsSeparator = false;
+        Key = key;
+        ModifierKey = modifier;
+    }
+
+    public static NodeMenuFlyoutItem Separator = new() { IsSeparator = true };
+    public readonly string Title;
+    public ICommand? Command;
+    public readonly NbtTagEnum CommandParameter;
+    public bool IsSeparator = false;
+    public readonly VirtualKey Key = VirtualKey.None;
+    public readonly VirtualKeyModifiers ModifierKey = VirtualKeyModifiers.None;
+}
